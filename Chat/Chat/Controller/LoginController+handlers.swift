aa223e516483017304b206a25d2b666ac99b67e5
Arthur <narimanovwork@icloude.com>
2019-08-20 18:39:20 +0300

    setup photoView and connect bd

--- Chat/Chat/Controller/LoginController+handlers.swift
+++ Chat/Chat/Controller/LoginController+handlers.swift
@@ -12,6 +12,58 @@ import FirebaseStorage
 
 extension LoginController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
+    
+    @objc func handleRegister() {
+        guard let email = emailTextField.text,
+            let password = passwordTextField.text,
+            let name = nameTextField.text else {
+                print("From is not valid")
+                return
+        }
+        
+        Auth.auth().createUser(withEmail: email, password: password) {
+            (user, error) in
+            if error != nil {
+                print(error as Any)
+                return
+            }
+            guard let uid = Auth.auth().currentUser?.uid else { return }
+            
+            //successfully authenticated user
+            let storageRef = Storage.storage().reference().child("myImage.png")
+            
+            if let uploadData = self.profileImageView.image?.pngData() {
+                
+                storageRef.putData(uploadData, metadata: nil, completion: { (metadata,  error) in
+                    if error != nil {
+                        print(error as Any)
+                        return
+                    }
+                    if let profileImageUrl = metadata?.path {
+                        let values = ["name": name, "email": email, "profileImageUrl": profileImageUrl]
+                        self.registerUserIntoDatabaseWithUID(uid: uid, values: values as [String : AnyObject])
+                    }
+                })
+            }
+        }
+    }
+    
+    private func registerUserIntoDatabaseWithUID(uid: String, values: [String: AnyObject]) {
+        
+        var ref: DatabaseReference!
+        ref = Database.database().reference(fromURL: "https://chat-35ca2.firebaseio.com/")
+        let usersReference = ref.child("users").child(uid)
+
+        usersReference.updateChildValues(values, withCompletionBlock: { (err, ref) in
+            if err != nil {
+                print(err as Any)
+                return
+            }
+            
+            self.dismiss(animated: true, completion: nil)
+        })
+    }
+    
     @objc func handSelectProfileImageView() {
         let picker = UIImagePickerController()
         
@@ -42,37 +94,4 @@ extension LoginController: UIImagePickerControllerDelegate, UINavigationControll
         dismiss(animated: true, completion: nil)
     }
     
-    @objc func handleRegister() {
-        guard let email = emailTextField.text,
-            let password = passwordTextField.text,
-            let name = nameTextField.text else {
-                print("From is not valid")
-                return
-        }
-        
-        Auth.auth().createUser(withEmail: email, password: password) {
-            (user, error) in
-            if error != nil {
-                print(error as Any)
-                return
-            }
-            guard let uid = Auth.auth().currentUser?.uid else { return }
-            
-            //successfully authenticated user
-//            let storageRef = S
-            
-            var ref: DatabaseReference!
-            ref = Database.database().reference(fromURL: "https://chat-35ca2.firebaseio.com/")
-            let usersReference = ref.child("users").child(uid)
-            let values = ["name": name, "email": email]
-            usersReference.updateChildValues(values, withCompletionBlock: { (err, ref) in
-                if err != nil {
-                    print(err as Any)
-                    return
-                }
-                
-                self.dismiss(animated: true, completion: nil)
-            })
-        }
-    }
 }
