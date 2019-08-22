625aeb84a0c9002363bc44dd2ba66fbce14dfa7c
Arthur <narimanovwork@icloude.com>
2019-08-22 18:04:00 +0300

    Add imageView in message tab

--- Chat/Chat/Controller/NewMessageController.swift
+++ Chat/Chat/Controller/NewMessageController.swift
@@ -29,6 +29,8 @@ class NewMessageController: UITableViewController {
                 
                 user.name = dictionary["name"] as? String
                 user.email = dictionary["email"] as? String
+                user.profileImageUrl = dictionary["profileImageUrl"] as? String
+                
                 self.users.append(user)
                 
                 DispatchQueue.main.async {
@@ -48,23 +50,73 @@ class NewMessageController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        //let use a hack for now, we actually need to dequeue our cells for memory efficincy
-//        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: cellId)
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as! UserCell
         let user = users[indexPath.row]
         
         cell.textLabel?.text = user.name
         cell.detailTextLabel?.text = user.email
         
+        if let profileImageUrl = user.profileImageUrl {
+            let url = URL(string: profileImageUrl)
+            
+            URLSession.shared.dataTask(with: url!) { (data, response, error) in
+                if error != nil {
+                    print("Error: ---- ",error as Any)
+                    return
+                }
+                DispatchQueue.main.async {
+                    cell.profileImageView.image = UIImage(data: data!)
+                    print("____________________")
+                }
+            }.resume()
+        }
+        
         return cell
+    }
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 56
     }
 }
 
 class UserCell: UITableViewCell {
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        textLabel?.frame = CGRect(x: 56, y: textLabel!.frame.origin.y, width: textLabel!.frame.width, height: textLabel!.frame.height)
+
+        detailTextLabel?.frame = CGRect(x: 56, y: detailTextLabel!.frame.origin.y, width: detailTextLabel!.frame.width, height: detailTextLabel!.frame.height)
+    }
+    
+    let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        
+        imageView.contentMode = .scaleAspectFill
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.layer.cornerRadius = 20
+        imageView.layer.masksToBounds = true
+        
+        return imageView
+    }()
+    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
+        
+        addSubview(profileImageView)
+        
+        //ios 9 constraint anchors
+        //need x,y, width, height anchors
+        profileImageView.leftAnchor.constraint(equalTo: leftAnchor, constant: 8).isActive = true
+        profileImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        profileImageView.widthAnchor.constraint(equalToConstant: 40).isActive = true
+        profileImageView.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        
     }
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 }
+
+
+
+//let use a hack for now, we actually need to dequeue our cells for memory efficincy
+//        let cell = UITableViewCell(style: .subtitle, reuseIdentifier: cellId)
